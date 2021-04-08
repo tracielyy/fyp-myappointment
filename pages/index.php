743128372adab6099e2631cbcd98da5193dae9b7
Tracy <57853991+tracyling98@users.noreply.github.com>
2021-04-09 00:09:18 +0800

    Edit index.php

--- pages/index.php
+++ pages/index.php
@@ -5,10 +5,16 @@
 <html>
     <head>
         <title>FYP-21-S2-24</title>
+        <!-- Styling -->
+        
     </head>
     <body>
+        <!-- PHP Script -->
         <?php
             // Code here
         ?>
+        
+        <!-- HTML Design -->
+        
     </body>
 </html>
