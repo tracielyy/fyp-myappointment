aa8979a8a9a5abe1712bd1195674c37e961a315a
Tracy <57853991+tracieqwynn@users.noreply.github.com>
2021-04-22 00:31:58 +0800

    Add Appointment_Record

---
+++ entities/Appointment_Record.php
@@ -0,0 +1,26 @@
+<?php
+
+class Appointment_Record{
+    
+    // Properties
+    private string $appointmentRecordID;
+
+    // Constructor
+    function __construct() {
+
+    
+    }
+    
+    
+    // Getters
+    function getAppointmentRecordID () {
+        return $this->appointmentRecordID;
+    }
+    
+
+    // Setters
+    
+}
+
+
+?>
