27a70a02e584d134f58485397b9efa7ba731c5bb
Tracy <57853991+tracieqwynn@users.noreply.github.com>
2021-04-22 21:13:59 +0800

    Modify Registration Function
    - Added Some Validations (Not Fully Done)
    - Added Patient Insert Function.

--- entities/Patient.php
+++ entities/Patient.php
@@ -1,20 +1,33 @@
 <?php
 
-require_once '../entities/Appointment_Record';
+require_once '../entities/Appointment_Record.php';
 
-class Patient extends Account_User{
-    
+class Patient extends Account_User {
+
     // Properties
+    protected const PATIENT = "Patient";
+
     //private $appointment_record = array();  // Appointment_Record Object
-    
     // Constructor
     function __construct($firstname = NULL, $lastname = NULL, $gender = NULL, $dob = NULL,
             $contactnumber = NULL, $address = NULL, $email = NULL, $password = NULL) {
-        parent::__construct($firstname,$lastname, $gender, $dob, $contactnumber,$address, $email, $password );
-    
+        parent::__construct($firstname, $lastname, $gender, $dob, $contactnumber, $address, $email, $password);
     }
-    
-    
+
+    // Use For Debugging/ Logging Purpose
+    public function __toString() {
+        return parent::__toString();
+    }
+
+    //============================================
+    //      Methods Accessing Firestore Database 
+    //============================================
+    public static function add_patient(array $userDataArr) {
+        $db = new Database();
+        $userDataArr['usertype'] = self::PATIENT;
+        $db->insert_data(parent::ACCOUNT_USER, $userDataArr);
+    }
+
 //    // Getters
 //    function get_appointment_record() {
 //        return $this->$appointment_record;
@@ -26,6 +39,5 @@ class Patient extends Account_User{
 //        $this->$appointment_record = new Appointment_Record(); // Params TBC
 //    }
 }
-
 
 ?>
