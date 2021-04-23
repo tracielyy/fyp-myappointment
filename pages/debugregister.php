0a36a8f4c3e05d452fc3d5f722388bd73f9302e3
Tracy <57853991+tracieqwynn@users.noreply.github.com>
2021-04-23 22:04:40 +0800

    Minor format edit

--- pages/debugregister.php
+++ pages/debugregister.php
@@ -27,7 +27,7 @@ session_start();
 
             <!-- Debug Test For Users -->
             <!-- Hint: Explode & Implode For Date Of Birth (DD-MM-YYYY) If there is other preferred string format (e.g. '/') -->
-        <?php
+            <?php
             // Used to store correct data
             $registerArr = array(
                 'firstname' => '',
@@ -190,7 +190,7 @@ session_start();
                     echo "<div style='color:red;'>Register Fail!</div>";
                 }
             }
-        ?>
+            ?>
 
             <!-- Form -->
             <form  method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
@@ -207,16 +207,16 @@ session_start();
                 <!-- Gender -->
                 <label for="gender">Select Gender: </label>
                 <input type="radio" id="Female" name="gender" value="F"<?php
-            if ($registerArr['gender'] == "F") {
-                echo "checked";
-            }
-            ?>/><label for="Female" class="btnLabel">Female</label>
-
-                <input type="radio" name="gender" id="Male" value="M" <?php
-                if ($registerArr['gender'] == "M") {
+                if ($registerArr['gender'] == "F") {
                     echo "checked";
                 }
-            ?> /><label for="Male">Male</label>
+                ?>/><label for="Female" class="btnLabel">Female</label>
+
+                <input type="radio" name="gender" id="Male" value="M" <?php
+                       if ($registerArr['gender'] == "M") {
+                           echo "checked";
+                       }
+                       ?> /><label for="Male">Male</label>
                 </select><br/>
 
 
