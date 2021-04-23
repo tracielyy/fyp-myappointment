98556ec553811b2843cd15b10ae152ce6ca35b16
Tracy <57853991+tracieqwynn@users.noreply.github.com>
2021-04-23 22:48:56 +0800

    Added Comments Of Draft For Properties

--- entities/Appointment_Record.php
+++ entities/Appointment_Record.php
@@ -4,6 +4,10 @@ class Appointment_Record{
     
     // Properties
     private string $appointmentRecordID;
+    # Appointment Status (Missed, Open, etc) -- Need To Call A Function To Update Status When Directed To The Page or Reload
+    # Appointment Date
+    # Appointment Time
+    # Appointment Venue
 
     // Constructor
     function __construct() {
