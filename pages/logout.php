999a9e12489f4b7c84449c1bc89f4cfd4d71ac50
Tracy <57853991+tracieqwynn@users.noreply.github.com>
2021-04-23 22:06:58 +0800

    Added logout to update session

--- pages/logout.php
+++ pages/logout.php
@@ -1,17 +1,19 @@
 <!--
    Developed By FYP-21-S2-24
 -->
-<!-- This Will Remove User Session Information -->
+<!-- This File Is Solely Used For Debugging -->
 <?php
 session_start();
-
-// Make Sure User Is Logged In
+require_once '../entities/Account_User.php';
 if (isset($_SESSION['user'])) {
+    $current_user = unserialize($_SESSION['user']);
+    Account_User::logout($current_user->get_email());
     $_SESSION = array();
     session_destroy();
+    
+    echo "Successfully Logout";
 }
-// -- <Insert Redirecting Location Below> --
-
-
+// -- Insert Redirecting Location Below --
+//header("Location:debugIndex.php");
 ?>
 
