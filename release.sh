617edb3bfcfa36ea3bbdea099f9aba969b4e294b
Tracy <57853991+tracieqwynn@users.noreply.github.com>
2021-04-26 13:54:06 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Test Release"
-echo "extension=grpc.so" > "/conf/php/8/php-fpm.conf"
-echo "extension=grpc.so" > "/conf/php/php-fpm.conf"
+echo "extension=grpc.so" > "/vendor/heroku/heroku-buildpack-php/conf/php/8/php-fpm.conf"
+echo "extension=grpc.so" > "/vendor/heroku/heroku-buildpack-php/conf/php/php-fpm.conf"
