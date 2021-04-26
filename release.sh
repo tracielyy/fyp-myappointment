21ae31b03dbd48c9f51fc67d62da46c054a923d3
Tracy <57853991+tracieqwynn@users.noreply.github.com>
2021-04-26 16:24:14 +0800

    Update release.sh and test

--- release.sh
+++ release.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Test Release"
 echo "extension=grpc.so" > "/app/.heroku/php/etc/php/php.ini"
-#echo "extension=grpc.so" > "/app/.heroku/php/etc/php" # This is a directory
+echo "extension=grpc.so" > "/app/.heroku/php/etc/php/conf.d/000-heroku.ini" 
+echo "extension=grpc.so" > "/app/.heroku/php/etc/php/conf.d/010-ext-zend_opcache.ini"
