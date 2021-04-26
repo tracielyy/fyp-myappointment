85684d4234f517677644d9de2946157b5590dfb9
Tracy <57853991+tracieqwynn@users.noreply.github.com>
2021-04-26 14:40:46 +0800

    Update composer and release.sh

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Test Release"
-echo "extension=grpc.so" > "/vendor/heroku/heroku-buildpack-php/conf/php/8/php-fpm.conf"
-echo "extension=grpc.so" > "/vendor/heroku/heroku-buildpack-php/conf/php/php-fpm.conf"
+echo "extension=grpc.so" > "vendor/heroku/heroku-buildpack-php/support/build/_conf/php/7/0/conf.d/000-heroku.ini"
+echo "extension=grpc.so" > "vendor/heroku/heroku-buildpack-php/support/build/_conf/php/conf.d/000-heroku.ini"
