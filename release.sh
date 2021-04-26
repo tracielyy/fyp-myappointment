65256b4d32b1daabc50360b36b2e611c11f11944
Tracy <57853991+tracieqwynn@users.noreply.github.com>
2021-04-26 16:34:36 +0800

    Test grpc

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Test Release"
-echo "extension=grpc.so" > "/app/.heroku/php/etc/php/php.ini"
-echo "extension=grpc.so" > "/app/.heroku/php/etc/php/conf.d/000-heroku.ini" 
-echo "extension=grpc.so" > "/app/.heroku/php/etc/php/conf.d/010-ext-zend_opcache.ini"
+echo "extension_dir=.'/ext'"
+echo "extension=grpc" > "/app/.heroku/php/etc/php/php.ini"
+echo "extension=grpc" > "/app/.heroku/php/etc/php/conf.d/000-heroku.ini" 
+echo "extension=grpc" > "/app/.heroku/php/etc/php/conf.d/010-ext-zend_opcache.ini"
