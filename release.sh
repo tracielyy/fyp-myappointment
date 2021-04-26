e3790b672d000abe1c734590fa759a71ea454dd8
Tracy <57853991+tracieqwynn@users.noreply.github.com>
2021-04-26 16:06:51 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Test Release"
-echo "extension=grpc.so" > "vendor/heroku/heroku-buildpack-php/support/build/_conf/php/7/0/conf.d/000-heroku.ini"
-echo "extension=grpc.so" > "vendor/heroku/heroku-buildpack-php/support/build/_conf/php/conf.d/000-heroku.ini"
+echo "extension=grpc.so" > "/app/.heroku/php/etc/php/php.ini"
+echo "extension=grpc.so" > "/app/.heroku/php/etc/php"
