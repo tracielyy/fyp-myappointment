c9c08ce18b16cf41d3c80292a987688584a0034c
Tracy <57853991+tracieqwynn@users.noreply.github.com>
2021-05-06 23:50:48 +0800

    Update Medical_Personnel.php

--- resources/entities/Medical_Personnel.php
+++ resources/entities/Medical_Personnel.php
@@ -4,8 +4,8 @@ class Medical_Personnel extends Account_User {
 
     // Properties
     private string $medical_facility; // Name of the medical facility
-    
-    
+    private string $license_number; // License Number
+
     // Constructor
     public function __construct($firstname, $lastname, $gender, $dob,
             $contactnumber, $address, $usertype, $createdon, $email, $password = NULL) {
@@ -15,6 +15,14 @@ class Medical_Personnel extends Account_User {
     }
 
     // Getters
+    public function get_medical_facility() {
+        return $this->medical_facility;
+    }
+
+    public function get_license_number() {
+        return $this->license_number;
+    }
+
     // Setters
 }
 
