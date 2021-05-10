a0c35462085b954aed9761e2b595ce35cb19c2b0
Tracy <57853991+tracieqwynn@users.noreply.github.com>
2021-05-11 00:23:23 +0800

    Add enums path

--- resources/config.php
+++ resources/config.php
@@ -1,11 +1,9 @@
 <!-- Main Configuration File -->
 <?php
-
-
 /* Creating Constants For Heavily Used Paths */
-define("COMPONENT_PATH", realpath(dirname(__FILE__) .  '/components'));
+define("COMPONENT_PATH", realpath(dirname(__FILE__) . '/components'));
 define("ENTITIES_PATH", realpath(dirname(__FILE__) . '/entities'));
 define("UTILS_PATH", realpath(dirname(__FILE__) . '/utils'));
-
+define("ENUMS_PATH", realpath(dirname(__FILE__) . '/enums'));
 ?>
 
