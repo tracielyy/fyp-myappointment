91e172282240190c377509e2098d9dcf973130b0
nervurex <nervurex@gmail.com>
2021-05-13 21:52:34 +0800

    added link

--- resources/components/navbar.php
+++ resources/components/navbar.php
@@ -8,10 +8,6 @@
 <!-- Will Need To Add Logic To Make Sure User Is Logged In Before Displaying Logout Button. -->
 <?php
 echo '
- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
       <a class="navbar-brand mx-2" href="index.php">My Appointment</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
