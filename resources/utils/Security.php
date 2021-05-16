cd45b22ec2a08d9c1e966272fa4f76ef32c6eb94
Tracy <57853991+tracieqwynn@users.noreply.github.com>
2021-05-16 13:54:40 +0800

    Create Security.php
    - Security Base Template Created For Encryption and Hash Of Confidential Info

---
+++ resources/utils/Security.php
@@ -0,0 +1,24 @@
+<!-- 
+    Developed By FYP-21-S2-24
+-->
+
+<?php
+/*
+ * @author yanying (Tracy)
+ * @author nanta
+ */
+
+class Security {
+    
+    // Properties
+    
+    
+    // -- Constructor -- //
+    public function __construct(){
+        
+    }
+    
+    // -- Static Functions -- //
+    
+}
+?>
