a2756eb1cddd183572ac550fb057650294b801b3
Tracy <57853991+tracieqwynn@users.noreply.github.com>
2021-05-17 22:33:08 +0800

    Added functions to display upcoming appointments

--- public/debugviewappointments.php
+++ public/debugviewappointments.php
@@ -15,11 +15,20 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
         $user = unserialize($_SESSION["user"]);
         echo $user . "<br/>";
         $email['email'] = $user->get_email();
-        $upcoming_appt = Appointment_Record::get_upcoming_appointments($email);
-        if($upcoming_appt == NULL){
+        $upcoming_arr = Appointment_Record::get_upcoming_appointments($email);
+        if ($upcoming_arr == NULL) {
             echo "NUll";
         } else {
-            echo $upcoming_appt;
+            // For Each Upcoming Appointment Record
+            foreach ($upcoming_arr as $record) {
+                echo "<br/>--------------------<br/>";
+                echo $record; // Implicitly calling toString
+
+                echo "This is my appointment status " . $record->get_appointmentstatus(); // Return String
+                
+                // $record->get_facility(); will return `Medical_Facility` object
+                echo "Location Name: " . $record->get_facility()->get_address();
+            }
         }
     }
 }
