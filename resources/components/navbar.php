9dea889a4558a24338d5bcb6d553454b6a1c84da
Tracy <57853991+tracieqwynn@users.noreply.github.com>
2021-05-29 23:41:33 +0800

    Minor change to the url

--- resources/components/navbar.php
+++ resources/components/navbar.php
@@ -12,7 +12,7 @@ echo '
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-      <a class="navbar-brand mx-2" href="index.php">My Appointment</a>
+      <a class="navbar-brand mx-2" href="/">My Appointment</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
