a88bae9036e6e3cc71235e69cde096ba04676902
nervurex <nervurex@gmail.com>
2021-06-19 00:09:37 +0800

    added variable for navbar

--- public/createappointment.php
+++ public/createappointment.php
@@ -10,7 +10,10 @@ require_once ENUMS_PATH . '/User_Type.php';
 require_once ENUMS_PATH . '/Appointment_Type.php';
 require_once FUNCTIONS_PATH . '/PatientFunctions.php';
 require_once FUNCTIONS_PATH . '/AccountUserFunctions.php';
+$pageName = 'createappointment'
 ?>
+
+
 
 <html>
 
