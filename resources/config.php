7f943a648a50a21140440c1b606716fa1ea2f6f5
Tracie <57853991+tracieqwynn@users.noreply.github.com>
2021-06-22 19:01:54 +0800

    Change Name Of "components" to "templates"

--- resources/config.php
+++ resources/config.php
@@ -1,6 +1,6 @@
 <?php
 /*  Main Configuration File: Creating Constants For Heavily Used Paths */
-define("COMPONENTS_PATH", realpath(dirname(__FILE__) . '/components'));
+define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/templates'));
 define("ENTITIES_PATH", realpath(dirname(__FILE__) . '/entities'));
 define("UTILS_PATH", realpath(dirname(__FILE__) . '/utils'));
 define("ENUMS_PATH", realpath(dirname(__FILE__) . '/enums'));
