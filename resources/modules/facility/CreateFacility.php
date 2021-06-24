1bb1c482fbbdd5282743ca8c7a258f652d8df52b
Tracie <57853991+tracieqwynn@users.noreply.github.com>
2021-06-24 13:21:35 +0800

    Folder Rename

---
+++ resources/modules/facility/CreateFacility.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * @author yanying (Tracie)
+ */
+
+/* Load Config File */
+require_once '../resources/config.php';
+
+/*
+ *       # ADD MEDICAL FACILITY #
+ */
+
+class CreateFacility{
+    
+    // -- GENERATE FACILITY ID
+    
+    // -- CREATE NEW MEDICAL FACILTY
+    
+    
+    
+}
+?>
