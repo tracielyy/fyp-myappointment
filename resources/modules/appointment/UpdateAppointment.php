765dd34bfda19001a5b48db3e11ce55d6187f674
Tracie <57853991+tracieqwynn@users.noreply.github.com>
2021-06-24 12:14:38 +0800

    Class Created For Reschedule Appointment

---
+++ resources/modules/appointment/UpdateAppointment.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * @author yanying (Tracie)
+ */
+
+/* Load Config File */
+require_once '../resources/config.php';
+require_once APPT_MOD . '/Appointment_Record.php';
+require_once APPT_MOD . '/Appointment_Slot.php';
+
+/*
+ *       # RESCHEDULE APPOINTMENT#
+ */
+
+class UpdateAppointment{
+    
+}
+?>
