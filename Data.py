9b6249d23159a742189a36b1fab4ac1a40cf3e01
clement <clementleekh@gmail.com>
2021-06-24 22:03:14 +0800

    commit

--- Data.py
+++ Data.py
@@ -1,20 +1,68 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import time, datetime
 
 cred = credentials.Certificate("fyp-21-s2-24-firebase-adminsdk-7qer9-ad53d1c1e2.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-emp_ref = db.collection('Medical_Facility').document('mf002').collection('Doctor Consultation').document('21-06-2021').collection('Slots')
-docs = emp_ref.get()
+doctor_slots = db.collection('Account_User').document('Medical_Personnel-iBnhkCP6HAhM0MvxeI4O').collection('Appointment_Slots').document('22-06-2021').collection('Slots')
+timing = doctor_slots.get()
 
-for doc in docs:
-    print('{} => {} '.format(doc.id, doc.to_dict()))
+for slots in timing:
+    print('{} => {} '.format(slots.id, slots.to_dict()))
 
-# #adding first data
-# doc_ref = db.collection('Medical_Facility').document('mf002')
-#
-# doc_ref.set({
-#
-# })
+#adding first data
+data = {
+    'avaliable' : True,
+    'patient' : ['test'],
+    'slotid' : 'slot-1002',
+    'time' : '0930'
+}
+
+#doctor_slots.document('slot-1002').set(data)
+d = datetime.datetime.now()
+
+doctor = {
+    'accountdetails' : {
+        'createdon' : {
+            'date' : d.strftime("%d-%m-%Y"),
+            'time' : d.strftime("%X")}
+    },
+    'credentials' : {
+        'email' : '',
+        'password' : ''
+    },
+    'passwordreset' : {
+        'passwordtoken' : '',
+        'requestedon' : {
+            'date' : d.strftime("%d-%m-%Y"),
+            'time' : d.strftime("%X")
+        },
+        'tokenused' : False
+    },
+    'practionerinfo' : {
+        'facilityid' : ['mf001'],
+        'licensenumber' : 'doc-002',
+        'specialisation' : 'Neurology'
+    },
+    'profile' : {
+        'address' : '35 Tampines Road',
+        'contactnumber' : '98752364',
+        'dob' : '03-05-1989',
+        'gender' : 'F',
+        'name' : {
+            'first' : 'John',
+            'last' : 'Doe'
+        }
+    },
+    'session' : {
+        'ipaddress' : '192.168.1.194',
+        'isloggedin' : False,
+        'sessionid' : 'kthlla5tf4gsf4rj04fdjibb0m',
+        'token' : 'xe,yE0DtLEwoWH1'}
+}
+
+for x in range (1,5):
+    db.collection('Account_User').document('MedicalPersonnel00' + str(x)).set(doctor)
--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import schedule
 import time
+from datetime import datetime
 
 def job():
-    print("I'm working...")
+    print("Time now: ")
 
-schedule.every(10).seconds.do(job)
+schedule.every(1).seconds.do(job)
 schedule.every(10).minutes.do(job)
-schedule.every().hour.do(job)
-schedule.every().day.at("10:30").do(job)
-schedule.every(5).to(10).minutes.do(job)
-schedule.every().monday.do(job)
-schedule.every().wednesday.at("13:15").do(job)
-schedule.every().minute.at(":17").do(job)
+schedule.every().day.at("00:00").do(job)
 
 while True:
     schedule.run_pending()
+    print(datetime.now())
     time.sleep(1)
