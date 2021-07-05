111a191feed08a2c068a31f917f4b421086865d2
clement <clementleekh@gmail.com>
2021-07-05 16:32:55 +0800

    test

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import time
 from datetime import datetime
 
 def job():
-    print("Time now: ")
+    print("Time now: " + datetime.now())
 
-schedule.every(1).seconds.do(job)
 schedule.every(10).minutes.do(job)
 schedule.every().day.at("00:00").do(job)
 
 while True:
     schedule.run_pending()
-    print(datetime.now())
     time.sleep(1)
