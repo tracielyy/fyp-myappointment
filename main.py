249be8a110e793314e0d4454fde88176073a57ea
clement <clementleekh@gmail.com>
2021-07-05 18:49:43 +0800

    timezone

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ import datetime
 
 timezone = pytz.timezone('Asia/Singapore')
 zone = timezone.localize(datetime.datetime.now())
-print(timezone)
 
 def job():
     print("Time now: " + str(zone))
-
+    
 schedule.every(10).minutes.do(job)
 schedule.every().day.at("00:00").do(job)
 
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
