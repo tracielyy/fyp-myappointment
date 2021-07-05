6aec2afbb83d413eca4386adc988fd94df65b578
clement <clementleekh@gmail.com>
2021-07-05 17:07:49 +0800

    error changes

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import schedule
 import time
-from datetime import datetime
+import pytz
+import datetime
 
 def job():
     print("Time now: " + str(datetime.now()))
 
 schedule.every(10).minutes.do(job)
 schedule.every().day.at("00:00").do(job)
+
+timezone = pytz.timezone("Singapore")
+zone = timezone.localize(datetime.datetime.now())
 
 while True:
     schedule.run_pending()
