96f127d0bf2759cc28a584670f470346b926d0eb
clement <clementleekh@gmail.com>
2021-07-06 18:05:50 +0800

    test

--- main.py
+++ main.py
@@ -2,11 +2,27 @@ import schedule
 import time
 from pytz import timezone
 from datetime import datetime
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import Data as data
+from Data import doctor
+
+db = firestore.client()
 
 def job():
     datenow = datetime.now()
     datetime_tz = timezone('Asia/Singapore').localize(datenow)
     print("Time now: " + str(datetime_tz))
+
+def add_data(x):
+    db.collection('Account_User').document('MedicalPersonnel0' + str(x)).set(doctor)
+    print("Data Added")
+
+for i in range(10,15):
+    i = 10
+    schedule.every(30).minutes.do(add_data,i)
+    i+=1
 
 schedule.every(10).minutes.do(job)
 schedule.every().day.at("00:00").do(job)
@@ -14,3 +30,4 @@ schedule.every().day.at("00:00").do(job)
 while True:
     schedule.run_pending()
     time.sleep(1)
+    
