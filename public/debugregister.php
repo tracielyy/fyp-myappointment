f438d49cf7dc6973b08dbe46283f0a87c9add162
Tracie <57853991+tracieqwynn@users.noreply.github.com>
2021-07-20 21:27:41 +0800

    Fix autoload issue for firestore client
    - Require front-end to add nric field

--- public/debugregister.php
+++ public/debugregister.php
@@ -2,6 +2,7 @@
 session_start();
 /* Load Config File */
 require_once '../resources/config.php';
+require '../vendor/autoload.php';
 require_once USER_MOD . '/Account_User.php';
 require_once USER_MOD . '/Patient.php';
 require_once UTIL_MOD . '/Regex.php';
