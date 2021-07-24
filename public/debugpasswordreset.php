bd7427c16e924d84bbfaa73ad736aa48553bc00c
nervurex <nervurex@gmail.com>
2021-07-24 14:25:38 +0800

    trying stuff, better not change to the new one

--- public/debugpasswordreset.php
+++ public/debugpasswordreset.php
@@ -120,7 +120,7 @@ setcookie($url_name, $url_value, time() + 3600);
 
 
             /* ------------ End Validation ------------ */
-            if (!in_array(FALSE, $validArr)) {
+            if (in_array(FALSE, $validArr)) {
                 echo "Password Pass";
 
                 // -- Store The Password To Database -- //
