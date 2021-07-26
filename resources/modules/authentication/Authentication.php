0dd75d0e5c07ee37f45f9787b91404a12e7a6432
nervurex <nervurex@gmail.com>
2021-07-26 17:19:12 +0800

    not much changes

--- resources/modules/authentication/Authentication.php
+++ resources/modules/authentication/Authentication.php
@@ -85,7 +85,7 @@ class Authentication {
             if( $pass_authenticate == 'CORRECT_PASSWORD')
             {
                 return True;
-            } elseif($pass_authenticate == 'CORRECT_PASSWORD')
+            } elseif($pass_authenticate == 'INCORRECT_PASSWORD')
             {
                 return False;
             }
--- resources/modules/user/Patient.php
+++ resources/modules/user/Patient.php
@@ -120,7 +120,7 @@ class Patient extends Normal_User {
             {
                 //Create hash on the password (salt is already generated in the function)
                 $patient_info[$field] = $sec->hash($value); 
-            }elseif ($field == 'NRIC')
+            }elseif ($field == 'nric')
             {
                 $patient_info[$field] = $sec->encrypt($value); 
             }
