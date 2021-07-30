5b7e977ebd2c38284d33bcd3ddb3bc1fba60a57d
Tracie <57853991+tracieqwynn@users.noreply.github.com>
2021-07-30 22:48:15 +0800

    name change

--- public/admin/s/create/newfacility.php
+++ public/admin/s/create/newfacility.php
@@ -10,13 +10,14 @@ require VENDOR_PATH . '/autoload.php';
 /*
  * CREATE FACILITY
  */
+
 ?><!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>List Of Facilities</title>
+        <title>Add New Facility</title>
         <?php require TEMPLATES_PATH . '/bootstrap.php' ?>
     </head>
     <body>
