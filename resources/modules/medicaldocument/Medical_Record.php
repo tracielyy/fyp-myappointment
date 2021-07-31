76484da5bb2ae1a0e0e91d7a8445d84ddb545f05
Tracie <57853991+tracieqwynn@users.noreply.github.com>
2021-07-31 22:33:23 +0800

    Update Medical_Record.php

--- resources/modules/medicaldocument/Medical_Record.php
+++ resources/modules/medicaldocument/Medical_Record.php
@@ -153,8 +153,8 @@ class Medical_Record {
             'createdon' => ['date' => $createdon->get_date(), 'time' => $createdon->get_time()],
             'appointmenttype' => $medical_record_info['appointmenttype'],
             'practitioner' => $medical_record_info['practitioner'],
-            'diagnosisdesc' => $medical_record_info['diagnosisdesc'],
-            'prescriptions' => $medical_record_info['prescriptions']
+            'diagnosisdesc' => "",
+            'prescriptions' => []
         );
 
         $doc_ref->set($medical_record_arr);
