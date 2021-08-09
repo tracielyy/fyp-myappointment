cb2580ac23342a8aea231491b6a6af10b55b71a5
nervurex <nervurex@gmail.com>
2021-08-09 22:35:33 +0800

    added working article and list

--- public/healthlist.php
+++ public/healthlist.php
@@ -2,17 +2,37 @@
 
 <?php
     session_start();
-    require_once '../resources/config.php';
-    // require_once ENTITIES_PATH . '/Account_User.php';
-    // require_once ENTITIES_PATH . '/Appointment_Record.php';
-    // require_once ENUMS_PATH . '/User_Type.php';
-    // require_once FUNCTIONS_PATH . '/PatientFunctions.php';
-    // require_once FUNCTIONS_PATH . '/AccountUserFunctions.php';
+    require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/resources/config.php';
+    require VENDOR_PATH . '/autoload.php';
+    require_once USER_MOD . '/Account_User.php';
+    require_once USER_MOD . '/Patient.php';
+    require_once USER_MOD . '/Medical_Personnel.php';
+    require_once USER_MOD . '/Facility_Admin.php';
+    require_once USER_MOD . '/Super_Admin.php';
+
+    require_once APPT_MOD . '/Appointment_Record.php';
+    require_once ENUMS_PATH . '/User_Type.php';
+
+    require_once ENUMS_PATH . '/Health_Info_Type.php';
+    require_once HINFO_MOD . '/Health_Info.php';
+    require_once UTIL_MOD . '/StringUtils.php';
     ?>
 
 <?php
     include TEMPLATES_PATH . '/bootstrap.php';
-    include_once TEMPLATES_PATH . '/navbar.php';
+    if (isset($_SESSION["user"])):
+
+    $user = unserialize($_SESSION["user"]);
+        include TEMPLATES_PATH . '/navbar-loggedin.php';
+    else:
+        include TEMPLATES_PATH . '/navbar.php';
+    endif;
+    $all_health_articles = Health_Info::retrieve_all_healthinfo();
+    $healtharticles_count = count($all_health_articles);
+    $healtharticles_divisible_by_six = $healtharticles_count % 6;
+
+
+
     ?>
 <html lang="en">
 
@@ -32,251 +52,91 @@
 </head>
 
 <body>
-    <div class="container">
-
-        <h1 class="mb-3"> Health Information and Tips</h1>
+    <div class="container my-4">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Article List</li>
+            </ol>
+        </nav>
+        <h1 class="mb-3 display-4">Health Information and Tips</h1>
 
         <div class="row">
             <div class="col">
-                <section name="FEATURED">
-                    <div class="card">
-                        <div class="card-body rounded" style="background-color: #5BA33F;">
-                            <b class="text-white"><i class="fas fa-star me-1"></i></i>Featured</b>
-                        </div>
-                    </div>
 
-                    <!-- Card -->
-                    <div class="card mh-100">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-1 min-vw-1000">
-                                    <i class="fas fa-shield-virus fa-3x opt-icon"></i>
-                                </div>
-                                <div class="col">
-                                    <h5 class="card-title">Symptoms of COVID-19: How to identify and to treat <span
-                                            class="badge bg-warning">World Health Notice</span></h5>
-                                    <p class="card-text line-clamp" style="margin:2px">Health problems, even minor ones,
-                                        can
-                                        interfere with
-                                        or even
-                                        overshadow other aspects of your life. Even relatively minor health issues such
-                                        as aches, pains, lethargy, and indigestion take a toll on your happiness and
-                                        stress levels. One way to improve your ability to cope with stress and feel
-                                        better is</p>
-                                    <label class="card-text text-muted" style="margin:0">World Health Organization -
-                                        Uploaded 5
-                                        months ago</label>
+                <section name="LATEST">
+                    <div class="row mt-3">
+                        <div class="col">
+                            <div class="card">
+                                <div class="card-body rounded" style="background-color: #3353CE;">
+                                    <b class="text-white"><i class="fas fa-info-circle me-2"></i>Latest</b>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <!-- End Card -->
 
-                    <!-- Card -->
-                    <div class="card mh-100">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-1 min-vw-1000">
-                                    <i class="fas fa-user-md fa-3x opt-icon"></i>
-                                </div>
-                                <div class="col">
-                                    <h5 class="card-title">Why You Should Take Care of Your Body and Health <span
-                                            class="badge bg-primary">Doctor's Advice</span></h5>
-                                    <p class="card-text line-clamp" style="margin:2px">Health problems, even minor ones,
-                                        can
-                                        interfere with
-                                        or even
-                                        overshadow other aspects of your life. Even relatively minor health issues such
-                                        as aches, pains, lethargy, and indigestion take a toll on your happiness and
-                                        stress levels. One way to improve your ability to cope with stress and feel
-                                        better is</p>
-                                    <label class="card-text text-muted" style="margin:0">Dr. Sylvester Stallone -
-                                        Uploaded 5
-                                        months ago</label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Card -->
+                            <!-- Card -->
+                            <?php 
+                            
+                                     
+                            foreach($all_health_articles as $article):
+                            
+                                $desc = substr($article->get_descriptions(),0,500);
+                                $desc = trim(preg_replace('/\s+/', ' ', $desc));
+                                $type = $article->get_type();
+                                if ($type == 'World Health Notice')
+                                {
+                                    $badge = '<span class="badge bg-warning ms-2">World Health Notice</span>';
+                                    $icon = '<i class="fas fa-shield-virus fa-3x opt-icon"></i>';
+                                } else if ($type == "Doctor's Advice")
+                                {
+                                    $badge = '<span class="badge bg-primary ms-2">Doctor\'s Advice</span>';
+                                    $icon = '<i class="fas fa-user-md fa-3x opt-icon"></i>';
+                                }   
+                                else if($type == 'Health Tips')
+                                {
+                                    $badge = '<span class="badge bg-success ms-2">Health Tips</span>';
+                                    $icon = '<i class="fas fa-plus-square fa-3x opt-icon"></i>';
+                                }
+                                echo '<div class="card mh-100">';
+                                echo '<div class="card-body">';
+                                echo '<div class="row">';
+                                echo '<div class="col-1 min-vw-1000">';
+                                echo $icon;
+                                echo '</div>';
+                                echo '<div class="col">';
+                                echo '<h5 class="card-title">'.$article->get_title().$badge.'</h5>';
+                                echo '<p class="card-text line-clamp" style="margin:2px">'.$desc.'</p>';
+                                echo '<a href="article.php?id='.$article->get_id().'" class="card-link stretched-link"><small class="text-muted">Click here to read more...</small></a>';
+                                echo '<br><small class="text-muted">Created on '.$article->get_createdon()->get_date().'</small>';
+                                if($article->get_createdon()->get_date() != $article->get_updatedon()->get_date())
+                                {
+                                    echo '<small class="text-muted"> • Updated on '.$article->get_updatedon()->get_date().'</small>';
+                                }
+                                echo '</div></div></div></div>';
+                                $nextid = $article->get_id();
+                            endforeach;
+                            $loadmore = array();
+                            $loadmore = Health_Info::retrieve_all_healthinfo($nextid);
+                            echo var_dump($loadmore);
+                            ?>
 
-                    <!-- Card -->
-                    <div class="card mh-100">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-1 min-vw-1000">
+                </section>
 
-                                    <i class="fas fa-plus-square fa-3x opt-icon"></i>
 
-                                </div>
-                                <div class="col">
-                                    <h5 class="card-title">Why You Should Take Care of Your Body and Health <span
-                                            class="badge bg-success">Health Tips</span></h5>
-                                    <p class="card-text line-clamp" style="margin:2px">Health problems, even minor ones,
-                                        can
-                                        interfere with
-                                        or even
-                                        overshadow other aspects of your life. Even relatively minor health issues such
-                                        as aches, pains, lethargy, and indigestion take a toll on your happiness and
-                                        stress levels. One way to improve your ability to cope with stress and feel
-                                        better is</p>
-                                    <label class="card-text text-muted" style="margin:0">Dr. Sylvester Stallone -
-                                        Uploaded 5
-                                        months ago</label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
             </div>
         </div>
-        <!-- End Card -->
-        </section>
-
-        <!-- FEATURED LIST END-->
-
-        <section name="LATEST">
-            <div class="row mt-3">
-                <div class="col">
-                    <div class="card">
-                        <div class="card-body rounded" style="background-color: #3353CE;">
-                            <b class="text-white"><i class="fas fa-info-circle me-2"></i>Latest</b>
-                        </div>
-                    </div>
-
-                    <!-- Card -->
-                    <div class="card mh-100">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-1 min-vw-1000">
-
-                                    <i class="fas fa-user-md fa-3x opt-icon"></i>
-
-                                </div>
-                                <div class="col">
-                                    <h5 class="card-title">Why You Should Take Care of Your Body and Health <span
-                                            class="badge bg-primary">Doctor's Advice</span></h5>
-                                    <p class="card-text line-clamp" style="margin:2px">Health problems, even minor ones,
-                                        can
-                                        interfere with
-                                        or even
-                                        overshadow other aspects of your life. Even relatively minor health issues such
-                                        as aches, pains, lethargy, and indigestion take a toll on your happiness and
-                                        stress levels. One way to improve your ability to cope with stress and feel
-                                        better is</p>
-                                    <label class="card-text text-muted" style="margin:0">Dr. Sylvester Stallone -
-                                        Uploaded 5
-                                        months ago</label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Card -->
-
-                    <!-- Card -->
-                    <div class="card mh-100">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-1 min-vw-1000">
-
-                                    <i class="fas fa-plus-square fa-3x opt-icon"></i>
-
-                                </div>
-                                <div class="col">
-                                    <h5 class="card-title">Why You Should Take Care of Your Body and Health <span
-                                            class="badge bg-success">Health Tips</span></h5>
-                                    <p class="card-text line-clamp" style="margin:2px">Health problems, even minor ones,
-                                        can
-                                        interfere with
-                                        or even
-                                        overshadow other aspects of your life. Even relatively minor health issues such
-                                        as aches, pains, lethargy, and indigestion take a toll on your happiness and
-                                        stress levels. One way to improve your ability to cope with stress and feel
-                                        better is</p>
-                                    <label class="card-text text-muted" style="margin:0">Dr. Sylvester Stallone -
-                                        Uploaded 5
-                                        months ago</label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Card -->
-
-                    <!-- Card -->
-                    <div class="card mh-100">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-1 min-vw-1000">
-
-                                    <i class="fas fa-plus-square fa-3x opt-icon"></i>
-
-                                </div>
-                                <div class="col">
-                                    <h5 class="card-title">Why You Should Take Care of Your Body and Health <span
-                                            class="badge bg-success">Health Tips</span></h5>
-                                    <p class="card-text line-clamp" style="margin:2px">Health problems, even minor ones,
-                                        can
-                                        interfere with
-                                        or even
-                                        overshadow other aspects of your life. Even relatively minor health issues such
-                                        as aches, pains, lethargy, and indigestion take a toll on your happiness and
-                                        stress levels. One way to improve your ability to cope with stress and feel
-                                        better is</p>
-                                    <label class="card-text text-muted" style="margin:0">Dr. Sylvester Stallone -
-                                        Uploaded 5
-                                        months ago</label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Card -->
-
-                    <!-- Card -->
-                    <div class="card mh-100">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-1 min-vw-1000">
-
-                                    <i class="fas fa-plus-square fa-3x opt-icon"></i>
-                                </div>
-                                <div class="col">
-                                    <h5 class="card-title">Why You Should Take Care of Your Body and Health <span
-                                            class="badge bg-success">Health Tips</span></h5>
-                                    <p class="card-text line-clamp" style="margin:2px">Health problems, even minor ones,
-                                        can
-                                        interfere with
-                                        or even
-                                        overshadow other aspects of your life. Even relatively minor health issues such
-                                        as aches, pains, lethargy, and indigestion take a toll on your happiness and
-                                        stress levels. One way to improve your ability to cope with stress and feel
-                                        better is</p>
-                                    <label class="card-text text-muted" style="margin:0">Dr. Sylvester Stallone -
-                                        Uploaded 5
-                                        months ago</label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Card -->
-                </div>
-        </section>
-
-        <nav aria-label="Page navigation example">
-            <ul class="pagination justify-content-end">
-                <li class="page-item disabled">
-                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item">
-                    <a class="page-link" href="#">Next</a>
-                </li>
-            </ul>
-        </nav>
+        <?php
+        if(true): ?>
+        <div class="row mt-4">
+            <div class="col text-center">
+                <form>
+                    <button class="btn btn-primary btn-lg text-center">
+                        Load More Articles
+                    </button>
+                </form>
+            </div>
+        </div>
+        <?php endif; ?>
     </div>
-
-
-    </div>
-
 
 
 </body>
